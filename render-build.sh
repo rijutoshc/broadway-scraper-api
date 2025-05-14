f46dbd9e28768c927438d22fde8fb6ea733b47a1
rijutoshc <144230315+rijutoshc@users.noreply.github.com>
2025-05-14 16:27:01 +0530

    Update render-build.sh to install Chromium driver

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
-
+#!/bin/bash
+# Update package list and install Chromium and ChromeDriver
 apt-get update && apt-get install -y chromium chromium-driver
+
+# Install Python dependencies
+pip install -r requirements.txt
